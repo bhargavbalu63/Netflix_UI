41097f2f3bde45bd175503ecf429b03b45e1c7a4
Bhargav <bhargavbalu63@gmail.com>
2025-07-09 14:22:08 +0530

    added nav bar logo and avatar

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Navbar.css"
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+
+      <img className='nav_logo' alt='netflix logo' src='https://www.freepnglogos.com/uploads/netflix-logo-0.png' />
+      <img className='nav_avatar' alt='avatar' src='https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg' />  
+      
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/request.js
@@ -0,0 +1,7 @@
+const API= "S54moGQHDE2LPf7r5n4wn6coABgleqWxnTwgvG0F"
+
+const request= {
+   
+
+}
+export default request
